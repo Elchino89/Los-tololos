9b26126112dc0cd8b9b3cc1359664513b242cce4
Elchino89 <sebagonz89@gmail.com>
2015-06-02 21:56:48 -0300

    retytryr
    sdrtytrt

--- app/src/main/java/com/example/chino/lostololos/MapsActivity.java
+++ app/src/main/java/com/example/chino/lostololos/MapsActivity.java
@@ -17,7 +17,7 @@ public class MapsActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         setUpMapIfNeeded();
-        //todos putos
+        //todrfgdfos pfdgdfutosggfdgdf
     }
 
     @Override
